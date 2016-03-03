c9b3bf593b05fff992330ab170f9d3945e2189bc
Eric Lagarda <portalhack.es@gmail.com>
2016-03-03 04:11:38 -0500

    Applied fixes from StyleCI

--- src/resources/lang/es/kgallery.php
+++ src/resources/lang/es/kgallery.php
@@ -1,6 +1,6 @@
 <?php
 
-return array (
+return  [
 
     'title'     =>  'KGallery',
     'menu_gal'  =>  'Ver Galerías',
@@ -11,69 +11,69 @@ return array (
     'update'    =>  'Actualizar galería',
     'create'    =>  'Crear galería',
     'search'    =>  'Buscar',
-    'headers'   =>  array(
+    'headers'   =>  [
         'image'     =>  'Imagen',
         'title'     =>  'Título',
-        'categories'=>  'Categorías',
-        'options'   =>  'Opciones'
-    ),
-    'options'   =>  array(
+        'categories' =>  'Categorías',
+        'options'   =>  'Opciones',
+    ],
+    'options'   =>  [
         'see'       =>  'Ver fotos',
         'edit'      =>  'Editar',
-        'delete'    =>  'Eliminar'
-    ),
+        'delete'    =>  'Eliminar',
+    ],
     'name'      =>  'Nombre',
-    'name_place'=>  'Nombre de la Galería',
-    'categories'=>  array(
+    'name_place' =>  'Nombre de la Galería',
+    'categories' =>  [
         'title'     =>  'Categorías',
         'add'       =>  'Añadir categoría',
         'edit'      =>  'Editar categoría',
         'create'    =>  'Crear categoria',
-        'update'    =>  'Actualizar categoría'
-    ),
-    'upload'    =>  array(
+        'update'    =>  'Actualizar categoría',
+    ],
+    'upload'    =>  [
         'info'      =>  'Arrastra y suelta archivos aquí<br />o añade archivos usando el botón',
-        'button'    =>  'Click para elegir archivo/s'
-    ),
-    'popup'     =>  array(
+        'button'    =>  'Click para elegir archivo/s',
+    ],
+    'popup'     =>  [
         'title'     =>  '¿Estás seguro?',
         'info'      =>  'No podrás recuperar el archivo.',
         'cancel'    =>  'Cancelar',
         'delete'    =>  'Sí, eliminar',
         'deleted_t' =>  '¡Eliminada!',
         'deleted'   =>  'El archivo se ha eliminado',
-        'secure'    =>  'El archivo no se ha eliminado'
-    ),
-    'delete_gal'=>  array(
+        'secure'    =>  'El archivo no se ha eliminado',
+    ],
+    'delete_gal' =>  [
         'title'     =>  '¿Estás seguro?',
         'info'      =>  'Se borrarán todas las fotos de esta galería.',
         'cancel'    =>  'Cancelar',
         'delete'    =>  'Sí, eliminar',
         'deleted_t' =>  '¡Eliminada!',
         'deleted'   =>  'La galería junto con sus archivos se han eliminado',
-        'secure'    =>  'La galería no se ha eliminado'
-    ),
-    'delete_cat'=>  array(
+        'secure'    =>  'La galería no se ha eliminado',
+    ],
+    'delete_cat' =>  [
         'title'     =>  '¿Estás seguro?',
         'info'      =>  'Se borrará esta categoría.',
         'cancel'    =>  'Cancelar',
         'delete'    =>  'Sí, eliminar',
         'deleted_t' =>  '¡Eliminada!',
         'deleted'   =>  'La categoría se ha eliminado',
-        'secure'    =>  'La categoría no se ha eliminado'
-    ),
-    'reorder'   =>  array(
+        'secure'    =>  'La categoría no se ha eliminado',
+    ],
+    'reorder'   =>  [
         'title_ok'  =>  'Éxito',
         'success'   =>  'Fotos reordenadas correctamente',
-        'error'     =>  'Error al reordenar'
-    ),
-    'editable'  =>  array(
+        'error'     =>  'Error al reordenar',
+    ],
+    'editable'  =>  [
         'name'      =>  'Introduce un nombre',
-        'desc'      =>  'Introduce una pequeña introducción'
-    ),
-    'messages'  =>  array(
+        'desc'      =>  'Introduce una pequeña introducción',
+    ],
+    'messages'  =>  [
         'success'   =>  'Cambios guardados correctamente',
-        'error'     =>  'Error al intentar realizar el proceso'
-    ),
+        'error'     =>  'Error al intentar realizar el proceso',
+    ],
 
-);
+];
